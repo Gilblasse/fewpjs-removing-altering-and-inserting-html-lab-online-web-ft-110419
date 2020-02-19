e2b0a3d4ca24a902c3772dd855d6d80f94ce739c
Nethelbert <gilblasse@gmail.com>
2020-02-19 05:53:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ main.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.setAttribute('id','victory');
-newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.innerHTML = "Emmanuel is the champion";
 document.body.appendChild(newHeader);
